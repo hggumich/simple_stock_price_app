7840c53a88ff117e59a5629469c80d4ab9c8ccdc
Hector Garza <hggumich@gamil.com>
2021-02-04 19:50:04 -0500

    Initial Commit

---
+++ app.py
@@ -0,0 +1,18 @@
+import yfinance as yf
+import streamlit as st
+import pandas as pd
+
+st.write("""
+# Simple Stock Price App
+
+Shown are the stock closing price and volume!
+""")
+
+tickerSymbol = 'GM'
+
+tickerData = yf.Ticker(tickerSymbol)
+
+tickerDf = tickerData.history(period='id', start='2010-5-31',end='2020-5-31')
+
+st.line_chart(tickerDf.Close)
+st.line_chart(tickerDf.Volume)
